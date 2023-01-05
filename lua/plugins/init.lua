8391ee716e5dba9946311ed371648f83bee43950
YirsisHertz <yirsishertz02@gmail.com>
2023-01-05 01:41:48 -0600

    un espacio extra

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -216,6 +216,7 @@ if present then
 
   -- load packer init options
   local init_options = require("plugins.configs.others").packer_init()
+
   init_options = require("core.utils").load_override(init_options, "wbthomason/packer.nvim")
 
   packer.init(init_options)
